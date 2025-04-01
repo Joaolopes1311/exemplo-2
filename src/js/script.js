1c2536232652d14ed347bdf83320b375948b3d75
Joao <joaopedro2003morra@gmail.com>
2025-04-01 09:10:37 -0300

    declaraçao e variaveis clear

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+//DECLARAÇÕES E VARIÁVEIS
+var nome = "fiap";
+console.log(nome)
+
+let idade = 21
+console.log(idade)
+
+const sobrenome ="pedro"
+console.log (sobrenome)
+//undefined
+let aula;
+console.log(aula)
+//null
+let valor = null
+console.log(valor)
+
+let exemplo1={}
+
+let exemplo2=[]
